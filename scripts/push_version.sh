eab8723bc4f621641ae4ddca4e38e29a3697106d
Thomas Maurin <maur1th@users.noreply.github.com>
2019-03-20 20:08:01 -0700

    Fix push version script

--- scripts/push_version.sh
+++ scripts/push_version.sh
@@ -5,7 +5,7 @@ then
     sed -i "s/^version \{0,1\}=.*/version = \"${TRAVIS_TAG:1}\"/" Cargo.toml
     git config --local user.name "Travis CI"
     git config --local user.email "travis@travis-ci.org"
-    git add Cargo.toml
+    git add Cargo.*
     git commit --allow-empty -m "Bump version to $TRAVIS_TAG [skip ci]"
     git remote set-url origin https://${GITHUB_TOKEN}@github.com/maur1th/tfdoc.git
     git pull --rebase origin master
