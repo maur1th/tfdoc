1881c0ec8f9b6ce1a3d89eebf5f43c60ff3bee28
Thomas Maurin <maur1th@users.noreply.github.com>
2019-03-21 17:04:05 -0700

    Fix printer for resources

--- src/printer.rs
+++ src/printer.rs
@@ -4,9 +4,9 @@ pub fn render(result: &[DocItem]) {
     for item in result.iter().filter(|i| i.category == BlockType::Comment) {
         print_title_block(&item.description);
     }
-    print_body(result, "Resources", BlockType::Resource);
-    print_body(result, "Inputs", BlockType::Variable);
-    print_body(result, "Outputs", BlockType::Output);
+    print_resources(result, "Resources", BlockType::Resource);
+    print_interface(result, "Inputs", BlockType::Variable);
+    print_interface(result, "Outputs", BlockType::Output);
 }
 
 fn print_title_block(description: &[String]) {
@@ -17,15 +17,30 @@ fn print_title_block(description: &[String]) {
     }
 }
 
-fn print_body(result: &[DocItem], name: &str, variant: BlockType) {
-    for (index, item) in result.iter().filter(|i| i.category == variant).enumerate() {
-        if (index == 0 && item.description.len() > 0)
-            || (index == 0 && variant != BlockType::Resource)
-        {
+fn print_resources(result: &[DocItem], name: &str, variant: BlockType) {
+    for (index, item) in result
+        .iter()
+        .filter(|i| i.category == variant && i.description.len() > 0)
+        .enumerate()
+    {
+        if index == 0 {
             println!("\n## {}\n", name);
         }
         if item.description.len() > 0 || variant != BlockType::Resource {
             println!("* `{}`: {}", item.name, item.description.join(" "));
+        }
+    }
+}
+
+fn print_interface(result: &[DocItem], name: &str, variant: BlockType) {
+    for (index, item) in result.iter().filter(|i| i.category == variant).enumerate() {
+        if index == 0 {
+            println!("\n## {}\n", name);
+        }
+        if item.description.len() > 0 {
+            println!("* `{}`: {}", item.name, item.description.join(" "));
+        } else {
+            println!("* `{}`", item.name);
         }
     }
 }
